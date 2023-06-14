416d39588bdcc7e1c6c59fd7c6a0fb0fb0941b43
Sean <zhuqingyun@outlook.com>
2023-06-14 15:05:58 -0600

    Config SQL server connection

--- eTicket/Program.cs
+++ eTicket/Program.cs
@@ -1,10 +1,11 @@
 using eTicket.Data;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-builder.Services.AddDbContext<AppDbContext>();
+builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnectionString")));
 
 var app = builder.Build();
 
