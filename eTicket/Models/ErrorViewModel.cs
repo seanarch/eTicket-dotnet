233071f69ee7d7c28b5934798e6853fa266e6911
seanarch <zhuqingyun@outlook.com>
2023-06-14 11:41:20 -0600

    init

--- eTicket/Models/ErrorViewModel.cs
+++ eTicket/Models/ErrorViewModel.cs
@@ -1,4 +1,4 @@
-﻿namespace eTicket.Models;
+namespace eTicket.Models;
 
 public class ErrorViewModel
 {
@@ -6,4 +6,3 @@ public class ErrorViewModel
 
     public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
 }
-
